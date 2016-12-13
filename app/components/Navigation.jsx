d20a9cd7a515d0354801ee9172b67e86fe8dc257
kentah <kenthow@gmail.com>
2016-12-13 07:22:12 -0600

    Initial commit

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -7,7 +7,7 @@ var Navigation = () => {
     <div className='top-bar'>
       <div className='top-bar-left'>
         <ul className='menu'>
-          <li>
+          <li className='menu-text'>
             React Timer App
           </li>
           <li>
